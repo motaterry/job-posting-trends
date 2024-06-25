5e9fb9338214ff541c75f3926d90ac19deb73308
Terry Mota <102319588+motaterry@users.noreply.github.com>
2024-06-25 19:43:19 +0000

    update

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import fetch from 'node-fetch';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Serve the main HTML file for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/api/job-posts', async (req, res) => {
     const { jobTitle, timeframe } = req.query;
